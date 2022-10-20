9aca23774c38840063b141b4751eb51e161bb3c4
AitAmerMezianeAmine <amineait1986@gmail.com>
2022-10-20 10:50:22 +0200

    Initial Commit

---
+++ OR/ORApp.swift
@@ -0,0 +1,17 @@
+//
+//  ORApp.swift
+//  OR
+//
+//  Created by Amine Ait amer meziane on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
