f685a645849024c742ba8b45a3d712a6200099c8
AitAmerMezianeAmine <amineait1986@gmail.com>
2022-10-20 11:40:43 +0200

    ajout d'un fichier test

---
+++ OR/Preview Content/TestFile.swift
@@ -0,0 +1,10 @@
+//
+//  TestFile.swift
+//  OR
+//
+//  Created by Amine Ait amer meziane on 20/10/2022.
+//
+
+import Foundation
+
+//ceci est un commentaire
---
+++ OR/Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  OR
+//
+//  Created by Amine Ait amer meziane on 20/10/2022.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Test_Previews: PreviewProvider {
+    static var previews: some View {
+        Test()
+    }
+}
